7c1b465fc68ea24291b034463042cbbab84f7213
Ashwin10101010 <92063872+Ashwin10101010@users.noreply.github.com>
2023-05-23 23:46:12 -0400

    Add files via upload

---
+++ Nuclear/power.js
@@ -0,0 +1,74 @@
+function p1()
+{
+    window.location = "index.html";
+}
+
+function p2()
+{
+    window.location = "explain.html";
+}
+
+function p3() 
+{
+    window.location = "cost.html";
+}
+
+function p4()
+{
+    window.location = "inexpense.html";
+}
+
+function p5()
+{
+    window.location = "power.html";
+}
+
+function p6()
+{
+    window.location = "locateable.html";
+}
+
+function p7()
+{
+    window.location = "pleniful.html";
+}
+
+function p8()
+{
+    window.location = "reliable.html";
+}
+
+function p9() 
+{
+    window.location = "renewable.html";
+}
+
+function p10() 
+{
+    window.location = "gases.html";
+}
+
+function p11()
+{
+    window.location = "heat.html";
+}
+
+function p12()
+{
+    window.location = "pollution.html";
+}
+
+function p13()
+{
+    window.location = "loss.html";
+}
+
+function p14()
+{
+    window.location = "nimby.html";
+}
+
+function p15() 
+{
+    window.location = "end.html";
+}
